c8bedc17beac9ac03d66ae2e443558f142f7eeec
Carlota Bindner <cev.bindner@gmail.com>
2017-10-05 01:23:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,22 +20,7 @@ class Triangle
     elsif a <= 0 || b <= 0 || c <= 0
       raise TriangleError
     end
-      
   end
-  
-  def validation
-    valid_triangle = [(a + b < c),(a + c < b), (b + c < a)]
-    sides = [a, b, c]
-    sides.each do |s|
-      if s <= 0
-        valid_triangle << false
-      end
-    end
-    if valid_triangle.include(false)
-      raise TriangleError
-    end
-  end
-
 
 end
 
