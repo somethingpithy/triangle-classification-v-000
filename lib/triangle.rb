cc33f485eae3bcf29d5e8f071495d42cb229b724
Carlota Bindner <cev.bindner@gmail.com>
2017-10-05 01:40:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
   end
 
   def kind
-    if a == b && b == c
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a == b && b == c
       :equilateral
     elsif a == b || a == c || b == c
       :isosceles
@@ -16,7 +18,7 @@ class Triangle
       :scalene
     end
   end
-
+  
 end
 
 
